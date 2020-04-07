941464712edff2c899c7f5233ac11eb0749bf4d4
Zoey <zoeyruss@gmail.com>
2020-04-07 13:53:22 -0400

    back up push

--- js/buttons.js
+++ js/buttons.js
@@ -4,20 +4,25 @@
       $('.onchange').text('');
     });
     $('.btn').on('click', function() {
-      $('.change-head').text('Outcome');
+      $('.change-head').text('Mood');
       $('.onchange').text('');
     });
 
 
     $('.tbtn').on('click', function() {
-      $('.change-head').text('Process');
+      $('.change-head').text('Mood');
+      $('.onchange').text('');
+    });
+
+    $('.tbtn').on('click', function() {
+      $('.change-head-sm').text('process');
       $('.onchange').text('');
     });
        
  
     
     $('.tbtn').on('click', function() {
-      $('.change').text('Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in  dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with deskto');
+      $('.change').text('Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard consectetur, from a Lorem Ipsum passage, and going through the cites of the word in  dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with deskto');
       $('.onchange').text('');
     });
   
